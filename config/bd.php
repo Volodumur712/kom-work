22950db8f99880f49bf0a5820ae2e4052968b855
vova <citrus1989@gmail.com>
2023-01-24 19:36:18 +0200

    test

--- config/bd.php
+++ config/bd.php
@@ -1,13 +1,19 @@
 <?php
- $servername = "sql11.freemysqlhosting.net";
-     $database = "sql11591844";
-     $username = "sql11591844";
-     $password = "rPP8UYAFSx"; 
+  $servername = "sql207.epizy.com";
+      $database = "epiz_33448594_123";
+      $username = "epiz_33448594";
+      $password = "Pcgoy8OclCguS"; 
 
-    // $servername = "localhost";
-    // $database = "sql11591844";
-    // $username = "root";
-    // $password = ""; 
+     // заблокована
+    //  $servername = "sql11.freemysqlhosting.net";
+    //  $database = "sql11591844";
+    //  $username = "sql11591844";
+    //  $password = "rPP8UYAFSx"; 
+
+    //  $servername = "localhost";
+    //  $database = "122";
+    //  $username = "root";
+    //  $password = ""; 
     
     // створення зєднання
     $conn = mysqli_connect($servername, $username, $password, $database);
