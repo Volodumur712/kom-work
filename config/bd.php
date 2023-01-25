b62ab439ab6d9a53a684fd00f77e804b1fba694b
vova <citrus1989@gmail.com>
2023-01-25 12:41:32 +0200

    projje

--- config/bd.php
+++ config/bd.php
@@ -1,8 +1,14 @@
 <?php
-  // $servername = "sql207.epizy.com";
-  //     $database = "epiz_33448594_123";
-  //     $username = "epiz_33448594";
-  //     $password = "Pcgoy8OclCguS"; 
+// $servername = "sql207.epizy.com";
+//      $database = "epiz_33448594_123";
+//      $username = "epiz_33448594";
+//      $password = "Pcgoy8OclCguS";
+
+    // нова
+    // $servername = "sql207.epizy.com";
+    //  $database = "epiz_33448594_123";
+    //  $username = "epiz_33448594";
+    //  $password = "Pcgoy8OclCguS";
 
      // заблокована
     //  $servername = "sql11.freemysqlhosting.net";
@@ -11,10 +17,10 @@
     //  $password = "rPP8UYAFSx"; 
 
     // загальна
-      $servername = "localhost";
-      $database = "122";
-      $username = "root";
-      $password = ""; 
+      // $servername = "localhost";
+      // $database = "122";
+      // $username = "root";
+      // $password = ""; 
     
     // створення зєднання
     $conn = mysqli_connect($servername, $username, $password, $database);
