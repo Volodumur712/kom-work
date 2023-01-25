464263ecc393bc5694f156e9ca038c1a9caf16b2
ZarKa1985 <kz85@ukr.net>
2023-01-25 20:13:15 +0100

    about, menu, contacts

--- partials/pages/about.php
+++ partials/pages/about.php
@@ -1,21 +1,48 @@
-<?php
-require($_SERVER['DOCUMENT_ROOT'] . '/partials/header.php');
-
-require($_SERVER['DOCUMENT_ROOT'] . '/upload/upload.php');
-?>
+ 
 
 <section class="about">
-<h1>upload image</h1>
-    <div class="wrapper">
-        <?php if(!empty($statusMsg)) { ?>
-            <p class="status <?php echo $status; ?>"> <?php $statusMsg; ?> </p>
-        <?php } ?>
-        <form action="#" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-            <label for="image">Select Image File:</label>
-            <input type="file" name="image" class="form-control">
+ 
+<div class="about" id="about-bg">
+
+    <div class="container">
+        <div class="about-top">
+                        <h1>Let's get acquainted</h1>
         </div>
-        <input type="submit" name="submit" class="btn-primary" value="Upload">
-        </form>
+
+
+         <div class="about-us">
+            <div class="about-text">
+                     <div class="about-years">
+                        <div class="years">
+                            <h1>15</h1>
+                            <h5>Years</h5>
+                        </div>
+                        <h3>of working experience as a web designer &amp; developer</h3>
+                    </div>
+                    <p>Я и моя молодая команда делаем что-то важное</p>
+                    <p><i>* Afordable Prices</i></p>
+                    <p><i>* High Quality Product</i></p>
+                    <p><i>* On Time Project Delivery</i></p>
+                    <a href="#" class="btn-read">Read More</a>
+
+            </div>
+                            
+            <div class="about-foto">
+                
+                <div class="nashi-foto">
+                    <img src="./assets/image/about-1.png">
+                </div>
+                <div class="nashi-foto">
+                    <img src="./assets/image/about-2.png">
+                </div>
+
+            </div>
+        </div>
+                          
+                                
     </div>
+    
+</div>
+
+
 </section>
--- partials/pages/home.php
+++ partials/pages/home.php
@@ -1,3 +1,48 @@
 <section class="home">
-    <p>home pages</p>
+  
+    <div class="home" id="home-bg">
+        <div class="container">
+            <div class="home-person">
+                <div class="person-text">
+                    <h3>I'm</h3>
+                    <h1>Virtual Person</h1>
+                    <h2 class="typed-text-output d-inline">Web Developer</h2><span class="typed-cursor d-none typed-cursor--blink">|</span>
+                    <div class="typed-text d-none">Web Designer, Web Developer, Front End Developer, Apps Designer, Apps Developer</div>
+                    <div class="knopki-play">
+                        <a href="#" class="btn-dowload">Download CV</a>
+                        <button type="button" class="btn-play" data-src="https://freelancer.itbanda.pro/">
+                            <span></span>
+                        </button>
+                        <h5 class="playv">Play Video</h5>
+                    </div>
+                </div>
+                
+
+                <div class="person-foto">
+                    <img src="./assets/image/profile.png">
+                </div>
+            </div>
+        </div>
+
+<style type="text/css" data-typed-js-css="true">
+        .typed-cursor{
+          opacity: 1;
+        }
+        .typed-cursor.typed-cursor--blink{
+          animation: typedjsBlink 0.7s infinite;
+          -webkit-animation: typedjsBlink 0.7s infinite;
+                  animation: typedjsBlink 0.7s infinite;
+        }
+        @keyframes typedjsBlink{
+          50% { opacity: 0.0; }
+        }
+        @-webkit-keyframes typedjsBlink{
+          0% { opacity: 1; }
+          50% { opacity: 0.0; }
+          100% { opacity: 1; }
+        }
+      </style>
+            
+    </div>
+
 </section>
