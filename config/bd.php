96e0741fa4ae31bf034ead9cf7fea9154d9b76f2
ZarKa1985 <kz85@ukr.net>
2023-01-26 18:39:40 +0100

    testimon

--- config/bd.php
+++ config/bd.php
@@ -10,17 +10,10 @@
     //  $username = "epiz_33448594";
     //  $password = "Pcgoy8OclCguS";
 
-     // заблокована
-    //  $servername = "sql11.freemysqlhosting.net";
-    //  $database = "sql11591844";
-    //  $username = "sql11591844";
-    //  $password = "rPP8UYAFSx"; 
-
-    // загальна
-      $servername = "localhost";
-      $database = "122";
-      $username = "root";
-      $password = ""; 
+    $servername = "localhost";
+     $database = "122";
+     $username = "root";
+     $password = ""; 
     
     // створення зєднання
     $conn = mysqli_connect($servername, $username, $password, $database);
--- partials/pages/testimonial.php
+++ partials/pages/testimonial.php
@@ -1,5 +1,34 @@
 
 <section class="testimonial">
-    <p>testimonial</p>
+<div class="testimonial" id="testimonial-bg">
+
+<div class="container">
+        <div class="testimonial-top">
+                        <h1>Testimonial</h1>
+        </div>
+        <div class="slidic">
+            <div class="centr-testimonial">
+                                <img  src="./assets/image/testimonial-2.jpg" >
+                            <div class="testimonial-icon">
+                                    <i class="fa fa-quote-left text-primary"></i>
+                            </div>
+                        </div>
+                            <p>“I just wanted to share a quick note and let you know that you guys do a really good job. I’m glad I decided to work with you. It’s really great how easy your websites are to update and manage. I never have any problem at all.
+                            </br>
+                            My new site is so much faster and easier to work with than my old site. It used to take me an hour or more to update a page and I would still sometimes screw things up. 
+                            </br>
+
+                            Now it’s almost like having a designer right here with me. I just choose the page, make the change and click save. It’s so simple.
+                            </br>
+
+                            Thanks, guys!”.</p>
+                            <hr>
+                            <h5>Client Name</h5>
+                            <span><h4>Profession</h4></span>
+                       
+        </div>
+                        
+</div> 
+</div> 
 </section>
 
